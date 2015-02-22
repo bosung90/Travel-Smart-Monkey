f0d04b03d8833b3827065cfbe6c6eb3882a91d39
Lisa Wong <miss.lisa19@hotmail.com>
2015-02-21 22:36:52 -0800

    renamed fuel consumption files and added parser

---
+++ src/com/teamunion/travelsmartmonkey/server/CarParser.java
@@ -0,0 +1,45 @@
+package com.teamunion.travelsmartmonkey.server;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.teamunion.travelsmartmonkey.shared.CarData;
+
+public class CarParser {
+	
+	public ArrayList<CarData> cars = new ArrayList<CarData>();
+	
+	public ArrayList<CarData> parseFuelConsumption(String filepath){
+		try {
+			Scanner scanner = new Scanner(new FileReader(filepath));
+			String line;
+			CarData data;
+			scanner.nextLine();
+			scanner.nextLine();
+			while (scanner.hasNextLine()){
+				line = scanner.nextLine(); // get the line
+				String[] results = line.split(","); // split it on ,
+				if(results[0].trim().isEmpty()){
+					scanner.close();
+					return cars;					
+				}
+				double year = Double.parseDouble(results[0]);
+				String make = results[1];
+				String model = results[2];
+				double fuelConsumption = Double.parseDouble(results[10]);
+				double co2Emission = Double.parseDouble(results[12]);	
+				
+				// create the car data
+				data = new CarData(year,make,model,fuelConsumption,co2Emission);
+				cars.add(data);
+			}
+			scanner.close();
+		}
+		catch (Exception e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		return cars;
+	}
+
+}
